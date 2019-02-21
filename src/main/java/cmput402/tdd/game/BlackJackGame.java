735dd0eebb74e9868a53587b6bfd85a693eaa450
Struckdown <bfleysher@gmail.com>
2019-02-20 22:34:54 -0700

    RED BlackJack Tests

--- src/main/java/cmput402/tdd/game/BlackJackGame.java
+++ src/main/java/cmput402/tdd/game/BlackJackGame.java
@@ -22,4 +22,15 @@ public class BlackJackGame extends Game{
 		int newCard = rand.nextInt(10)+1; // Returns 1-10
 		values.add(newCard);
 	}
+
+	public void resetHand(){
+	}
+
+	public int getDealerHand(){
+		return 0;
+	}
+
+	public int compareHands(){
+		return 0;
+	}
 }
--- src/test/java/cmput402/tdd/game/BlackJackGameTest.java
+++ src/test/java/cmput402/tdd/game/BlackJackGameTest.java
@@ -32,5 +32,52 @@ public class BlackJackGameTest{
 
 	@Test
 	public void testStartingHand(){
+		BlackJackGame game = new BlackJackGame();
+		ArrayList<Integer> fakeHand = new ArrayList<Integer>();
+		fakeHand = game.getHand();
+		assert(fakeHand.size() == 0);
+	}
+
+	@Test
+	public void testRestartHand(){
+		BlackJackGame game = new BlackJackGame();
+	
+		assert(game.getHand().size() == 0);
+
+		game.addCard(3);
+		assert(game.getHand().size() == 1);
+
+		game.resetHand();
+//		assert(game.getHand().size() == 0);
+	}
+
+	@Test
+	public void testWinCompareHands(){
+		BlackJackGame game = mock(BlackJackGame.class);
+		game.addCard(8);
+		game.addCard(10);
+		when(game.getDealerHand()).thenReturn(17);
+		int winner = game.compareHands();
+		assert(winner == 1);
+	}
+
+	@Test
+	public void testTieCompareHands(){
+		BlackJackGame game = mock(BlackJackGame.class);
+		game.addCard(7);
+		game.addCard(10);
+		when(game.getDealerHand()).thenReturn(17);
+		int winner = game.compareHands();
+		assert(winner == 0);
+	}
+
+	@Test
+	public void testLoseCompareHands(){
+		BlackJackGame game = mock(BlackJackGame.class);
+		game.addCard(6);
+		game.addCard(10);
+		when(game.getDealerHand()).thenReturn(17);
+		int winner = game.compareHands();
+		assert(winner == -1);
 	}
 }
